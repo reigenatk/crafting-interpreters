5961a194a3f7602b7b12b4851e5624d121d5b45e
richard.ma <richard.ma@imanage.com>
2022-07-26 14:35:14 -0500

    starting scanner

--- clox/main.c
+++ clox/main.c
@@ -1,27 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+
+static char* readFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+    // sets file ptr to end of file
+    fseek(file, 0L, SEEK_END);
+    // gets the size of it (since cursor is at end its just the size of file)
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    // dynamically allocate some mem, we dunno how big the array
+    // needs to be till we've read the file so that's why we
+    // need to dynamically allocate here
+    char* buffer = (char*)malloc(fileSize + 1);
+
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    // then copy the entire file into this buffer and return it
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
+static void repl() {
+    char line[1024];
+    for (;;) {
+        printf("> ");
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        interpret(line);
+    }
+}
+
 int main(int argc, const char* argv[]) {
     // create VM
     initVM();
 
-    // create a chunk with some bytecode in it
-    Chunk c;
-    initChunk(&c);
-    int idx = addConstantToChunk(&c, 21);
-    writeChunk(&c, OP_CONSTANT, 1);
-    writeChunk(&c, idx, 1);
-    writeChunk(&c, OP_NEGATE, 2);
-    writeChunk(&c, OP_RETURN, 3);
-    // dissasembleChunk(&c, "test chunk");
+    if (argc == 1) {
+        // run command line thing
+        repl();
+    }
+    else if (argc == 2) {
+        // run file
+        runFile(argv[1]);
+    }
+    else {
+        fprintf(stderr, "Usage: clox [path]\n");
+        exit(64);
+    }
 
-    // ask the VM to interpret our bytecode!
-    interpretChunk(&c);
-
-    // deallocate everything
     freeVM();
-    freeChunk(&c);
     return 0;
 }
